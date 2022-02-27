9b878f22f8d45bccd9f3cd1e9defb9510809485f
sirak <samueladonai34@gmail.com>
2022-02-26 21:14:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// set handlebars middlware
+app.engine('handlebars',exphbs.engine());
+app.set('view engine', 'handlebars');
+
+const mama  = "mama";
+//set handle
+app.get('/', function (req,res){
+	res.render('home',{
+		stuff : mama });
+});
+
+//set  static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT,() => console.log('Server listening on port'+ PORT));
